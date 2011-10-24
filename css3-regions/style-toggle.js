903fcdd8e0cd19ab3cc1e176fd03c41e14ac6446
Vincent Hardy <vhardy@adobe.com>
2011-10-24 23:48:45 +0000

    Updated with recent comments and feedback. Changes added to log. Added alternate stylesheet

--- css3-regions/style-toggle.js
+++ css3-regions/style-toggle.js
@@ -1,5 +1,4 @@
 (function() {
-
     var defaultStyleText = 'default spec. style';
     var additionalStyleText = "additional spec. style";
     var toggle;
@@ -8,24 +7,32 @@
     function toggleStyle() {
         var st = document.getElementById('st');
 
-        if (st.disabled === true) {
-            st.disabled = false;
+        if (st.hasAttribute('disabled') === true) {
+            st.removeAttribute('disabled');
             toggle.textContent = defaultStyleText;
         } else {
-            st.disabled = true;
+            st.setAttribute('disabled', 'true');
             toggle.textContent = additionalStyleText;
         }
     }
 
     window.onload = function() {
+        var st = document.getElementById('st');
         var divHead = document.getElementById('div-head');
+        var defaultText = additionalStyleText;
+        
+        if (st.hasAttribute('disabled') === false) {
+            defaultText = defaultStyleText;
+        }
+        
         toggle = document.createElement('a');
 
 		divHead.insertBefore(toggle, divHead.firstChild);
-        toggle.textContent = defaultStyleText;
+        toggle.textContent = defaultText;
         toggle.setAttribute('class', 'toggle');
         toggle.setAttribute('href', '#');
-        toggle.addEventListener("DOMActivate", toggleStyle, false);
+        toggle.addEventListener("click", toggleStyle, false);
     };
+    
 })();
 
