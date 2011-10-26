57f786c6d98fc4e16d38f7f125ea6da3fdd64aaf
Vincent Hardy <vhardy@adobe.com>
2011-10-26 00:45:34 +0000

    First sketch of consolidated CSS transform specification

---
+++ css3-transforms/style-toggle.js
@@ -0,0 +1,37 @@
+(function() {
+    var defaultStyleText = 'default spec. style';
+    var additionalStyleText = "additional spec. style";
+    var toggle;
+
+
+    function toggleStyle() {
+        var st = document.getElementById('st');
+
+        if (st.hasAttribute('disabled') === true) {
+            st.removeAttribute('disabled');
+            toggle.textContent = defaultStyleText;
+        } else {
+            st.setAttribute('disabled', 'true');
+            toggle.textContent = additionalStyleText;
+        }
+    }
+
+    window.onload = function() {
+        var st = document.getElementById('st');
+        var divHead = document.getElementById('div-head');
+        var defaultText = additionalStyleText;
+        
+        if (st.hasAttribute('disabled') === false) {
+            defaultText = defaultStyleText;
+        }
+        
+        toggle = document.createElement('a');
+
+		divHead.insertBefore(toggle, divHead.firstChild);
+        toggle.textContent = defaultText;
+        toggle.setAttribute('class', 'toggle');
+        toggle.setAttribute('href', '#');
+        toggle.addEventListener("click", toggleStyle, false);
+    };
+    
+})();
