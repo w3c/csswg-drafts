76e770956aafbc897cd42c05d9d6aa6681c5a685
Vincent Hardy <vhardy@adobe.com>
2011-10-29 01:04:04 +0000

    Made style toggle work on FF/WebKit/Opera

--- css3-regions/style-toggle.js
+++ css3-regions/style-toggle.js
@@ -2,16 +2,17 @@
     var defaultStyleText = 'default spec. style';
     var additionalStyleText = "additional spec. style";
     var toggle;
-
-
+    
     function toggleStyle() {
         var st = document.getElementById('st');
 
         if (st.hasAttribute('disabled') === true) {
             st.removeAttribute('disabled');
+            st.disabled = false;
             toggle.textContent = defaultStyleText;
         } else {
             st.setAttribute('disabled', 'true');
+            st.disabled = true;
             toggle.textContent = additionalStyleText;
         }
     }
@@ -20,10 +21,6 @@
         var st = document.getElementById('st');
         var divHead = document.getElementById('div-head');
         var defaultText = additionalStyleText;
-        
-        if (st.hasAttribute('disabled') === false) {
-            defaultText = defaultStyleText;
-        }
         
         toggle = document.createElement('a');
 
