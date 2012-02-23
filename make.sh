0473babeeb3571980a9a48738580d2463d1ae7bd
Glenn Adams <glenn@skynav.com>
2012-02-23 16:56:19 -0700

    update editors, upgrade make process, add current TR WD

--- make.sh
+++
@@ -1,4 +0,0 @@
-echo "run preprocessor"
-python cssom-generate.py
-echo "run anolis"
-anolis --omit-optional-tags --quote-attr-values --w3c-compat --enable=xspecxref --enable=refs --w3c-shortname="cssom" --filter=".publish" Overview.src.html Overview.html
