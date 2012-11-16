7a9336424895829823618b994888c34dace801d9
Simon Sapin <simon.sapin@exyr.org>
2012-11-16 23:13:36 +0100

    [css3-syntax] Update railroad-diagrams.js (+ add a script to do it again.)
    --HG--
    extra : amend_source : 092a0ce9476ff18350899a72054aad1e0353e858

--- css3-syntax/railroad-diagrams.js
+++ css3-syntax/railroad-diagrams.js
@@ -4,6 +4,15 @@ var ARC_RADIUS = 10;
 var DIAGRAM_CLASS = 'railroad-diagram';
 var TRANSLATE_HALF_PIXEL = true;
 
+function subclassOf(baseClass, superClass) {
+	baseClass.prototype = Object.create(superClass.prototype);
+	baseClass.prototype.$super = superClass.prototype;
+}
+
+function unnull(/* children */) {
+	return [].slice.call(arguments).reduce(function(sofar, x) { return sofar !== undefined ? sofar : x; });
+}
+
 
 function SVG(name, attrs, text) {
 	attrs = attrs || {};
@@ -16,22 +25,73 @@ function SVG(name, attrs, text) {
 	return el;
 }
 
+function FakeSVG(tagName, attrs, text){
+	if(!(this instanceof FakeSVG)) return new FakeSVG(tagName, attrs, text);
+	if(text) this.children = text;
+	else this.children = [];
+	this.tagName = tagName;
+	this.attrs = unnull(attrs, {});
+	return this;
+};
+FakeSVG.prototype.format = function(x, y, width) {
+	// Virtual
+};
+FakeSVG.prototype.addTo = function(parent) {
+	if(parent instanceof FakeSVG) {
+		parent.children.push(this);
+		return this;
+	} else {
+		var svg = this.toSVG();
+		parent.appendChild(svg);
+		return svg;
+	}
+};
+FakeSVG.prototype.toSVG = function() {
+	var el = SVG(this.tagName, this.attrs);
+	if(typeof this.children == 'string') {
+		el.textContent = this.children;
+	} else {
+		this.children.forEach(function(e) {
+			el.appendChild(e.toSVG());
+		});
+	}
+	return el;
+};
+FakeSVG.prototype.toString = function() {
+	var str = '<' + this.tagName + ' ';
+	for(var attr in this.attrs) {
+		str += attr + '="' + this.attrs[attr] + '" ';
+	}
+	str += '>';
+	if(typeof this.children == 'string') {
+		str += this.children;
+	} else {
+		this.children.forEach(function(e) {
+			str += e;
+		});
+	}
+	str += '</' + this.tagName + '>';
+	return str;
+}
+
 function Path(x,y) {
 	if(!(this instanceof Path)) return new Path(x,y);
-	this.str = "M"+x+' '+y;
+	FakeSVG.call(this, 'path');
+	this.attrs.d = "M"+x+' '+y;
 }
+subclassOf(Path, FakeSVG);
 Path.prototype.m = function(x,y) {
-	this.str += 'm'+x+' '+y;
+	this.attrs.d += 'm'+x+' '+y;
 	return this;
 }
 Path.prototype.h = function(val) {
-	this.str += 'h'+val;
+	this.attrs.d += 'h'+val;
 	return this;
 }
 Path.prototype.right = Path.prototype.h;
 Path.prototype.left = function(val) { return this.h(-val); }
 Path.prototype.v = function(val) {
-	this.str += 'v'+val;
+	this.attrs.d += 'v'+val;
 	return this;
 }
 Path.prototype.down = Path.prototype.v;
@@ -50,45 +110,31 @@ Path.prototype.arc = function(sweep){
 	} else {
 		var cw = 0;
 	}
-	this.str += "a"+ARC_RADIUS+" "+ARC_RADIUS+" 0 0 "+cw+' '+x+' '+y;
+	this.attrs.d += "a"+ARC_RADIUS+" "+ARC_RADIUS+" 0 0 "+cw+' '+x+' '+y;
 	return this;
 }
-Path.prototype.toSVG = function() {
-	return SVG('path', {d:this.str+'h.5'});
+Path.prototype.format = function() {
+	this.attrs.d += 'h.5';
+	return this;
 }
-Path.prototype.addTo = function(container) {
-	container.appendChild(this.toSVG());
-	return container;
-}
-
-function unnull(/* children */) {
-	return [].slice.call(arguments).reduce(function(sofar, x) { return sofar !== undefined ? sofar : x; });
-}
-
 
 function wrapString(value) {
     return ((typeof value) == 'string') ? new Terminal(value) : value;
 }
 
-function wrapStringArray(value) {
-    for (var i = 0; i < value.length; i++) {
-        if ((typeof value[i]) == 'string') {
-            value[i] = new Terminal(value[i]);
-        }
-    }
-    return value;
-}
-
-function Diagram(children) {
+function Diagram(items) {
 	if(!(this instanceof Diagram)) return new Diagram([].slice.call(arguments));
-	this.children = wrapStringArray(children);
-	this.children.unshift(new Start);
-	this.children.push(new End);
-	this.width = this.children.reduce(function(sofar, el) { return sofar + el.width + (el.needsSpace?20:0)}, 0)+1;
-	this.up = this.children.reduce(function(sofar,el) { return Math.max(sofar, el.up)}, 0);
-	this.down = this.children.reduce(function(sofar,el) { return Math.max(sofar, el.down)}, 0);
+	FakeSVG.call(this, 'svg', {class: DIAGRAM_CLASS});
+	this.items = items.map(wrapString);
+	this.items.unshift(new Start);
+	this.items.push(new End);
+	this.width = this.items.reduce(function(sofar, el) { return sofar + el.width + (el.needsSpace?20:0)}, 0)+1;
+	this.up = this.items.reduce(function(sofar,el) { return Math.max(sofar, el.up)}, 0);
+	this.down = this.items.reduce(function(sofar,el) { return Math.max(sofar, el.down)}, 0);
+	this.formatted = false;
 }
-Diagram.prototype.toSVG = function(paddingt, paddingr, paddingb, paddingl) {
+subclassOf(Diagram, FakeSVG);
+Diagram.prototype.format = function(paddingt, paddingr, paddingb, paddingl) {
 	paddingt = unnull(paddingt, 20);
 	paddingr = unnull(paddingt, 20);
 	paddingb = unnull(paddingt, 20);
@@ -96,235 +142,265 @@ Diagram.prototype.toSVG = function(paddingt, paddingr, paddingb, paddingl) {
 	var x = paddingl;
 	var y = paddingt;
 	y += this.up;
-	var g = SVG('g', TRANSLATE_HALF_PIXEL ? {transform:'translate(.5 .5)'} : {});
-	for(var i = 0; i < this.children.length; i++) {
-		var child = this.children[i];
-		if(child.needsSpace) {
+	var g = FakeSVG('g', TRANSLATE_HALF_PIXEL ? {transform:'translate(.5 .5)'} : {});
+	for(var i = 0; i < this.items.length; i++) {
+		var item = this.items[i];
+		if(item.needsSpace) {
 			Path(x,y).h(10).addTo(g);
 			x += 10;
 		}
-		g.appendChild(child.toSVG(x, y, child.width));
-		x += child.width;
-		if(child.needsSpace) {
+		item.format(x, y, item.width).addTo(g);
+		x += item.width;
+		if(item.needsSpace) {
 			Path(x,y).h(10).addTo(g);
 			x += 10;
 		}
 	}
-	var svg = SVG('svg',{width:this.width+paddingl+paddingr, height:this.up+this.down+paddingt+paddingb, class:DIAGRAM_CLASS});
-	svg.appendChild(g);
-	return svg;
+	this.attrs.width = this.width + paddingl + paddingr;
+	this.attrs.height = this.up + this.down + paddingt + paddingb;
+	g.addTo(this);
+	this.formatted = true;
+	return this;
 }
-Diagram.prototype.addTo = function(el) {
-	el = el || document.body;
-	return el.appendChild(this.toSVG());
+Diagram.prototype.addTo = function(parent) {
+	parent = parent || document.body;
+	this.$super.addTo.call(this, parent);
+}
+Diagram.prototype.toSVG = function() {
+	if (!this.formatted) {
+		this.format();
+	}
+	return this.$super.toSVG.call(this);
+}
+Diagram.prototype.toString = function() {
+	if (!this.formatted) {
+		this.format();
+	}
+	this.$super.toString.call(this);
 }
 
-function Sequence(children) {
+function Sequence(items) {
 	if(!(this instanceof Sequence)) return new Sequence([].slice.call(arguments));
-	this.children = wrapStringArray(children);
-	this.width = this.children.reduce(function(sofar, el) { return sofar + el.width + (el.needsSpace?20:0)}, 0);
-	this.up = this.children.reduce(function(sofar,el) { return Math.max(sofar, el.up)}, 0);
-	this.down = this.children.reduce(function(sofar,el) { return Math.max(sofar, el.down)}, 0);
+	FakeSVG.call(this, 'g');
+	this.items = items.map(wrapString);
+	this.width = this.items.reduce(function(sofar, el) { return sofar + el.width + (el.needsSpace?20:0)}, 0);
+	this.up = this.items.reduce(function(sofar,el) { return Math.max(sofar, el.up)}, 0);
+	this.down = this.items.reduce(function(sofar,el) { return Math.max(sofar, el.down)}, 0);
 }
-Sequence.prototype.toSVG = function(x,y,width) {
+subclassOf(Sequence, FakeSVG);
+Sequence.prototype.format = function(x,y,width) {
 	var diff = width - this.width;
-	var g = SVG('g');
-	Path(x,y).h(diff/2).addTo(g);
+	Path(x,y).h(diff/2).addTo(this);
 	x += diff/2;
-	for(var i = 0; i < this.children.length; i++) {
-		var child = this.children[i];
-		if(child.needsSpace) {
-			Path(x,y).h(10).addTo(g);
+	for(var i = 0; i < this.items.length; i++) {
+		var item = this.items[i];
+		if(item.needsSpace) {
+			Path(x,y).h(10).addTo(this);
 			x += 10;
 		}
-		g.appendChild(child.toSVG(x, y, child.width));
-		x += child.width;
-		if(child.needsSpace) {
-			Path(x,y).h(10).addTo(g);
+		item.format(x, y, item.width).addTo(this);
+		x += item.width;
+		if(item.needsSpace) {
+			Path(x,y).h(10).addTo(this);
 			x += 10;
 		}
 	}
-	Path(x,y).h(diff/2).addTo(g);
-	return g;
+	Path(x,y).h(diff/2).addTo(this);
+	return this;
 }
 
-function Choice(normal, children) {
+function Choice(normal, items) {
 	if(!(this instanceof Choice)) return new Choice(normal, [].slice.call(arguments,1));
+	FakeSVG.call(this, 'g');
 	this.normal = normal;
-	this.children = wrapStringArray(children);
-	this.width = this.children.reduce(function(sofar, el){return Math.max(sofar, el.width)},0) + ARC_RADIUS*4;
+	this.items = items.map(wrapString);
+	this.width = this.items.reduce(function(sofar, el){return Math.max(sofar, el.width)},0) + ARC_RADIUS*4;
 	this.up = this.down = 0;
-	for(var i = 0; i < children.length; i++) {
-		var child = children[i];
-		if(i < normal) { this.up += Math.max(ARC_RADIUS,child.up + child.down + VERTICAL_SEPARATION); }
-		if(i == normal) { this.up += Math.max(ARC_RADIUS, child.up); this.down += Math.max(ARC_RADIUS, child.down); }
-		if(i > normal) { this.down += Math.max(ARC_RADIUS,VERTICAL_SEPARATION + child.up + child.down); }
+	for(var i = 0; i < this.items.length; i++) {
+		var item = this.items[i];
+		if(i < normal) { this.up += Math.max(ARC_RADIUS,item.up + item.down + VERTICAL_SEPARATION); }
+		if(i == normal) { this.up += Math.max(ARC_RADIUS, item.up); this.down += Math.max(ARC_RADIUS, item.down); }
+		if(i > normal) { this.down += Math.max(ARC_RADIUS,VERTICAL_SEPARATION + item.up + item.down); }
 	}
 }
-Choice.prototype.toSVG = function(x,y,width) {
-	var g = SVG('g');
-
-	var last = this.children.length -1;
+subclassOf(Choice, FakeSVG);
+Choice.prototype.format = function(x,y,width) {
+	var last = this.items.length -1;
 	var innerWidth = this.width - ARC_RADIUS*4;
 
 	// Hook up the two sides if this is narrower than its stated width.
 	var diff = width - this.width;
-	Path(x,y).h(diff/2).addTo(g);
-	Path(x+diff/2+this.width,y).h(diff/2).addTo(g);
+	Path(x,y).h(diff/2).addTo(this);
+	Path(x+diff/2+this.width,y).h(diff/2).addTo(this);
 	x += diff/2;
 
 	// Do the elements that curve above
 	for(var i = this.normal - 1; i >= 0; i--) {
-		var child = this.children[i];
+		var item = this.items[i];
 		if( i == this.normal - 1 ) {
-			var distanceFromY = Math.max(ARC_RADIUS*2, this.children[i+1].up + VERTICAL_SEPARATION + child.down);
+			var distanceFromY = Math.max(ARC_RADIUS*2, this.items[i+1].up + VERTICAL_SEPARATION + item.down);
 		}
-		Path(x,y).arc('se').up(distanceFromY - ARC_RADIUS*2).arc('wn').addTo(g);
-		g.appendChild(child.toSVG(x+ARC_RADIUS*2,y - distanceFromY,innerWidth));
-		Path(x+ARC_RADIUS*2+innerWidth, y-distanceFromY).arc('ne').down(distanceFromY - ARC_RADIUS*2).arc('ws').addTo(g);
-		distanceFromY += Math.max(ARC_RADIUS, child.up + VERTICAL_SEPARATION + (i == 0 ? 0 : this.children[i-1].down));
+		Path(x,y).arc('se').up(distanceFromY - ARC_RADIUS*2).arc('wn').addTo(this);
+		item.format(x+ARC_RADIUS*2,y - distanceFromY,innerWidth).addTo(this);
+		Path(x+ARC_RADIUS*2+innerWidth, y-distanceFromY).arc('ne').down(distanceFromY - ARC_RADIUS*2).arc('ws').addTo(this);
+		distanceFromY += Math.max(ARC_RADIUS, item.up + VERTICAL_SEPARATION + (i == 0 ? 0 : this.items[i-1].down));
 	}
 
 	// Do the straight-line path.
-	Path(x,y).right(ARC_RADIUS*2).addTo(g);
-	g.appendChild(this.children[this.normal].toSVG(x+ARC_RADIUS*2, y, innerWidth));
-	Path(x+ARC_RADIUS*2+innerWidth, y).right(ARC_RADIUS*2).addTo(g);
+	Path(x,y).right(ARC_RADIUS*2).addTo(this);
+	this.items[this.normal].format(x+ARC_RADIUS*2, y, innerWidth).addTo(this);
+	Path(x+ARC_RADIUS*2+innerWidth, y).right(ARC_RADIUS*2).addTo(this);
 
 	// Do the elements that curve below
 	for(var i = this.normal+1; i <= last; i++) {
-		var child = this.children[i];
+		var item = this.items[i];
 		if( i == this.normal + 1 ) {
-			var distanceFromY = Math.max(ARC_RADIUS*2, this.children[i-1].down + VERTICAL_SEPARATION + child.up);
+			var distanceFromY = Math.max(ARC_RADIUS*2, this.items[i-1].down + VERTICAL_SEPARATION + item.up);
 		}
-		Path(x,y).arc('ne').down(distanceFromY - ARC_RADIUS*2).arc('ws').addTo(g);
-		g.appendChild(child.toSVG(x+ARC_RADIUS*2, y+distanceFromY, innerWidth));
-		Path(x+ARC_RADIUS*2+innerWidth, y+distanceFromY).arc('se').up(distanceFromY - ARC_RADIUS*2).arc('wn').addTo(g);
-		distanceFromY += Math.max(ARC_RADIUS, child.down + VERTICAL_SEPARATION + (i == last ? 0 : this.children[i+1].up));
+		Path(x,y).arc('ne').down(distanceFromY - ARC_RADIUS*2).arc('ws').addTo(this);
+		item.format(x+ARC_RADIUS*2, y+distanceFromY, innerWidth).addTo(this);
+		Path(x+ARC_RADIUS*2+innerWidth, y+distanceFromY).arc('se').up(distanceFromY - ARC_RADIUS*2).arc('wn').addTo(this);
+		distanceFromY += Math.max(ARC_RADIUS, item.down + VERTICAL_SEPARATION + (i == last ? 0 : this.items[i+1].up));
 	}
 
-	return g;
+	return this;
 }
 
-function Optional(child) {
-	return Choice(1, Skip(), child);
+function Optional(item, skip) {
+	if( skip === undefined )
+		return Choice(1, Skip(), item);
+	else if ( skip === "skip" )
+		return Choice(0, Skip(), item);
+	else
+		throw "Unknown value for Optional()'s 'skip' argument.";
 }
 
-function OneOrMore(child, rep) {
-	if(!(this instanceof OneOrMore)) return new OneOrMore(child, rep);
+function OneOrMore(item, rep) {
+	if(!(this instanceof OneOrMore)) return new OneOrMore(item, rep);
+	FakeSVG.call(this, 'g');
 	rep = rep || (new Skip);
-	this.child = child = wrapString(child);
-	this.rep = rep = wrapString(rep);
-	this.width = Math.max(child.width, rep.width) + ARC_RADIUS*2;
-	this.up = child.up;
-	this.down = Math.max(ARC_RADIUS*2, child.down + VERTICAL_SEPARATION + rep.up + rep.down);
+	this.item = wrapString(item);
+	this.rep = wrapString(rep);
+	this.width = Math.max(this.item.width, this.rep.width) + ARC_RADIUS*2;
+	this.up = this.item.up;
+	this.down = Math.max(ARC_RADIUS*2, this.item.down + VERTICAL_SEPARATION + this.rep.up + this.rep.down);
 }
+subclassOf(OneOrMore, FakeSVG);
 OneOrMore.prototype.needsSpace = true;
-OneOrMore.prototype.toSVG = function(x,y,width) {
-	var g = SVG('g');
-
+OneOrMore.prototype.format = function(x,y,width) {
 	// Hook up the two sides if this is narrower than its stated width.
 	var diff = width - this.width;
-	Path(x,y).h(diff/2).addTo(g);
-	Path(x+diff/2+this.width,y).h(diff/2).addTo(g);
+	Path(x,y).h(diff/2).addTo(this);
+	Path(x+diff/2+this.width,y).h(diff/2).addTo(this);
 	x += diff/2;
 
-	// Draw child
-	Path(x,y).right(ARC_RADIUS).addTo(g);
-	g.appendChild(this.child.toSVG(x+ARC_RADIUS,y,this.width-ARC_RADIUS*2));
-	Path(x+this.width-ARC_RADIUS,y).right(ARC_RADIUS).addTo(g);
+	// Draw item
+	Path(x,y).right(ARC_RADIUS).addTo(this);
+	this.item.format(x+ARC_RADIUS,y,this.width-ARC_RADIUS*2).addTo(this);
+	Path(x+this.width-ARC_RADIUS,y).right(ARC_RADIUS).addTo(this);
 
 	// Draw repeat arc
-	var distanceFromY = Math.max(ARC_RADIUS*2, this.child.down+VERTICAL_SEPARATION+this.rep.up);
-	Path(x+ARC_RADIUS,y).arc('nw').down(distanceFromY-ARC_RADIUS*2).arc('ws').addTo(g);
-	g.appendChild(this.rep.toSVG(x+ARC_RADIUS, y+distanceFromY, this.width - ARC_RADIUS*2));
-	Path(x+this.width-ARC_RADIUS, y+distanceFromY).arc('se').up(distanceFromY-ARC_RADIUS*2).arc('en').addTo(g);
+	var distanceFromY = Math.max(ARC_RADIUS*2, this.item.down+VERTICAL_SEPARATION+this.rep.up);
+	Path(x+ARC_RADIUS,y).arc('nw').down(distanceFromY-ARC_RADIUS*2).arc('ws').addTo(this);
+	this.rep.format(x+ARC_RADIUS, y+distanceFromY, this.width - ARC_RADIUS*2).addTo(this);
+	Path(x+this.width-ARC_RADIUS, y+distanceFromY).arc('se').up(distanceFromY-ARC_RADIUS*2).arc('en').addTo(this);
 
-	return g;
+	return this;
 }
 
-function ZeroOrMore(child, rep) {
-	return Optional(OneOrMore(child, rep));
+function ZeroOrMore(item, rep, skip) {
+	return Optional(OneOrMore(item, rep), skip);
 }
-ZeroOrMore.prototype = Object.create(OneOrMore.prototype);
-
 
 function Start() {
 	if(!(this instanceof Start)) return new Start();
+	FakeSVG.call(this, 'path');
 	this.width = 20;
 	this.up = 10;
 	this.down = 10;
 }
-Start.prototype.toSVG = function(x,y) {
-	return SVG('path', {d: 'M '+x+' '+(y-10)+' v 20 m 10 -20 v 20 m -10 -10 h 20.5'});
+subclassOf(Start, FakeSVG);
+Start.prototype.format = function(x,y) {
+	this.attrs.d = 'M '+x+' '+(y-10)+' v 20 m 10 -20 v 20 m -10 -10 h 20.5';
+	return this;
 }
 
 function End() {
 	if(!(this instanceof End)) return new End();
+	FakeSVG.call(this, 'path');
 	this.width = 20;
 	this.up = 10;
 	this.down = 10;
 }
-End.prototype.toSVG = function(x,y) {
-	return SVG('path', {d: 'M '+x+' '+y+' h 20 m -10 -10 v 20 m 10 -20 v 20'});
+subclassOf(End, FakeSVG);
+End.prototype.format = function(x,y) {
+	this.attrs.d = 'M '+x+' '+y+' h 20 m -10 -10 v 20 m 10 -20 v 20';
+	return this;
 }
 
 function Terminal(text) {
 	if(!(this instanceof Terminal)) return new Terminal(text);
+	FakeSVG.call(this, 'g');
 	this.text = text;
 	this.width = text.length * 8 + 20; /* Assume that each char is .5em, and that the em is 16px */
 	this.up = 11;
 	this.down = 11;
 }
+subclassOf(Terminal, FakeSVG);
 Terminal.prototype.needsSpace = true;
-Terminal.prototype.toSVG = function(x, y, width) {
-	var g = SVG('g');
+Terminal.prototype.format = function(x, y, width) {
 	var diff = width - this.width;
-	Path(x,y).right(width).addTo(g);
-	g.appendChild(SVG('rect', {x:x+diff/2, y:y-11, width:this.width, height:this.up+this.down, rx:10, ry:10}));
-	g.appendChild(SVG('text', {x:x+width/2, y:y+4}, this.text));
-	return g;
+	Path(x,y).right(width).addTo(this);
+	FakeSVG('rect', {x:x+diff/2, y:y-11, width:this.width, height:this.up+this.down, rx:10, ry:10}).addTo(this);
+	FakeSVG('text', {x:x+width/2, y:y+4}, this.text).addTo(this);
+	return this;
 }
 
 function NonTerminal(text) {
 	if(!(this instanceof NonTerminal)) return new NonTerminal(text);
+	FakeSVG.call(this, 'g');
 	this.text = text;
 	this.width = text.length * 8 + 20;
 	this.up = 11;
 	this.down = 11;
 }
+subclassOf(NonTerminal, FakeSVG);
 NonTerminal.prototype.needsSpace = true;
-NonTerminal.prototype.toSVG = function(x, y, width) {
-	var g = SVG('g');
+NonTerminal.prototype.format = function(x, y, width) {
 	var diff = width - this.width;
-	Path(x,y).right(width).addTo(g);
-	g.appendChild(SVG('rect', {x:x+diff/2, y:y-11, width:this.width, height:this.up+this.down}));
-	g.appendChild(SVG('text', {x:x+width/2, y:y+4}, this.text));
-	return g;
+	Path(x,y).right(width).addTo(this);
+	FakeSVG('rect', {x:x+diff/2, y:y-11, width:this.width, height:this.up+this.down}).addTo(this);
+	FakeSVG('text', {x:x+width/2, y:y+4}, this.text).addTo(this);
+	return this;
 }
 
 function Comment(text) {
 	if(!(this instanceof Comment)) return new Comment(text);
+	FakeSVG.call(this, 'g');
 	this.text = text;
 	this.width = text.length * 7 + 10;
 	this.up = 11;
 	this.down = 11;
 }
+subclassOf(Comment, FakeSVG);
 Comment.prototype.needsSpace = true;
-Comment.prototype.toSVG = function(x, y, width) {
-	var g = SVG('g');
+Comment.prototype.format = function(x, y, width) {
 	var diff = width - this.width;
-	Path(x,y).right(diff/2).addTo(g);
-	Path(x+diff/2+this.width,y).right(diff/2).addTo(g)
-	g.appendChild(SVG('text', {x:x+width/2, y:y+5, class:'comment'}, this.text));
-	return g;
+	Path(x,y).right(diff/2).addTo(this);
+	Path(x+diff/2+this.width,y).right(diff/2).addTo(this)
+	FakeSVG('text', {x:x+width/2, y:y+5, class:'comment'}, this.text).addTo(this);
+	return this;
 }
 
 function Skip() {
 	if(!(this instanceof Skip)) return new Skip();
+	FakeSVG.call(this, 'g');
 	this.width = 0;
 	this.up = 0;
 	this.down = 0;
 }
-Skip.prototype.toSVG = function(x, y, width) {
-	return Path(x,y).right(width).addTo(SVG('g'));
+subclassOf(Skip, FakeSVG);
+Skip.prototype.format = function(x, y, width) {
+	Path(x,y).right(width).addTo(this);
+	return this;
 }
---
+++ css3-syntax/update-diagram-lib.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://github.com/tabatkins/railroad-diagrams/raw/master/railroad-diagrams.js -O railroad-diagrams.js
+wget https://github.com/tabatkins/railroad-diagrams/raw/master/railroad-diagrams.css -O railroad-diagrams.css
