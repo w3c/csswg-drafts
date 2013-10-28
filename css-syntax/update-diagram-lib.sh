560ce88d7743964c6a7f0e5f13ffd42a99c0b6ce
Tab Atkins Jr. <jackalmage@gmail.com>
2013-10-28 15:35:24 -0700

    [css-syntax] Switch to Bikeshed's new railroad-diagram-generating functionality.
    --HG--
    extra : rebase_source : 2b7af7b75b6d00c0f074719e755637cdccd19792

--- css-syntax/update-diagram-lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://github.com/tabatkins/railroad-diagrams/raw/master/railroad-diagrams.js -O railroad-diagrams.js
-wget https://github.com/tabatkins/railroad-diagrams/raw/master/railroad-diagrams.css -O railroad-diagrams.css
