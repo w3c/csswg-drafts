5f56f2e06cf8b6b3a52cccbc4a9d42e07d561a61
Sylvain Galineau <galineau@adobe.com>
2014-04-07 13:57:10 -0700

    [css-animations] Admin: update make-changelog.sh so it includes both Overview.bs and Overview.src.html history; re-gen ChangeLog
    --HG--
    extra : rebase_source : 3900cd8952f39176e65914de4e744a8c2a21cfd6

--- css-animations/make-changelog.sh
+++ css-animations/make-changelog.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-hg log --style="$(dirname "$0")/make-changelog.template" "$(dirname "$0")/Overview.src.html" | sed 's/^	/  /;s/^- \(\[css[a-z0-9-]*\]\)* /- /'
+hg log --style="$(dirname "$0")/make-changelog.template" "$(dirname "$0")/Overview.bs" "$(dirname "$0")/Overview.src.html" | sed 's/^	/  /;s/^- \(\[css[a-z0-9-]*\]\)* /- /'
