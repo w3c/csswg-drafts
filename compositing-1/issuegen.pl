02daa2ee51ba0d4505c458605d58cf52d0068cd5
Nikos Andronikos <nikos.andronikos@cisra.canon.com.au>
2014-11-26 10:10:38 +1100

    Changed issuegen to point to fxtf editors draft url rather than a broken css link

--- compositing-1/issuegen.pl
+++ compositing-1/issuegen.pl
@@ -113,7 +113,7 @@ sub header {
 
 <p>Last call document: <a href="$url">$url</a>
 
-<p>Editor's draft: <a href="http://dev.w3.org/csswg/$shortname/">http://dev.w3.org/csswg/$shortname/</a>
+<p>Editor's draft: <a href="http://dev.w3.org/fxtf/$shortname/">http://dev.w3.org/fxtf/$shortname/</a>
 
 <p>The following color coding convention is used for comments:</p>
 
