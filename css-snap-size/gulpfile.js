d2e9a253207c695e53c3203a4ee99b9a6860e103
Koji Ishii <kojiishi@gmail.com>
2016-02-29 14:22:00 +0900

    [css-snap-size] Minor change in preview script

--- css-snap-size/gulpfile.js
+++ css-snap-size/gulpfile.js
@@ -5,13 +5,13 @@ Installation:
 3. (optional) Install gulp globally so it'll be on your path:
    `sudo npm install -g gulp`
 
-Then type `gulp` and make edits `Overview.bs`
-to see in your browser.
+Then run `gulp` and save `Overview.bs`
+for gulp to run bikeshed and refresh your browser.
 */
 
 var gulp = require("gulp");
 
-gulp.task("default", ["browser-sync"]);
+gulp.task("default", ["preview"]);
 
 gulp.task("bikeshed", function () {
   return bikeshed();
@@ -23,7 +23,7 @@ gulp.task("watch", function () {
   });
 });
 
-gulp.task("browser-sync", ["watch"], function () {
+gulp.task("preview", ["bikeshed", "watch"], function () {
   var browserSync = require("browser-sync");
   browserSync({
     server: {
@@ -35,54 +35,59 @@ gulp.task("browser-sync", ["watch"], function () {
   });
 });
 
-var use_local_bikeshed = true;
+var try_local_bikeshed_first = true;
 
 function bikeshed() {
-  return new Promise(function (resolve, reject) {
-    if (use_local_bikeshed)
-      bikeshed_local_or_online_with_cb(resolve, reject);
-    else
-      bikeshed_online_with_cb(resolve, reject);
+  return new Promise(bikeshed_cb);
+}
+
+function bikeshed_cb(resolve, reject) {
+  if (!try_local_bikeshed_first)
+    return bikeshed_online_cb(resolve, reject);
+  bikeshed_local_cb(resolve, function (e) {
+    if (e.code == "ENOENT") { // bikeshed not installed locally.
+      console.log("Local bikeshed not found, switch to online version of bikeshed.");
+      try_local_bikeshed_first = false; // prefer online for future calls.
+      bikeshed_online_cb(resolve, reject);
+      return;
+    }
+    reject(e);
   });
 }
 
-function bikeshed_local_or_online_with_cb(resolve, reject) {
+function bikeshed_local_cb(resolve, reject) {
   var spawn = require('child_process').spawn;
   spawn("bikeshed", [], {
     stdio: "inherit"
   }).on("error", function (e) {
-    // ENOENT doesn't fire "close" so need to handle here.
-    // console.log("bikeshed error:", e);
-    if (e.code == "ENOENT") { // bikeshed not installed locally.
-      console.log("Local bikeshed not found, switch to online version of bikeshed.");
-      use_local_bikeshed = false; // prefer online for future calls.
-      bikeshed_online_with_cb(resolve, reject);
-    } else
-      reject(e);
+    // ENOENT doesn't fire "close" and throws without on("error")
+    reject(e);
   }).on("close", function (code) {
-    if (!code)
-      return resolve();
-    console.log("Local bikeshed exited with code:", code);
-    // No need to reject() because on("error") also fires.
+    if (code) {
+      console.log("Local bikeshed exited with code:", code);
+      // No need to reject() because on("error") also fires.
+      return;
+    }
+    return resolve();
   });
 }
 
-function bikeshed_online_with_cb(resolve, reject) {
+function bikeshed_online_cb(resolve, reject) {
   var fs = require("fs");
   var request = require('request');
   // gulp.watch() kicks in when pipe() creates the file,
   // so write to a temp file and move it.
-  var tmp = "~Overview.html";
-  var req = request.post({
+  var tmpfile = "~Overview.html";
+  request.post({
     url: "http://api.csswg.org/bikeshed/",
     formData: {
       file: fs.createReadStream("Overview.bs"),
     },
   }).on("error", function (err) {
     reject(err);
-  }).pipe(fs.createWriteStream(tmp))
+  }).pipe(fs.createWriteStream(tmpfile))
   .on("finish", function () {
-    fs.rename(tmp, "Overview.html", function (err) {
+    fs.rename(tmpfile, "Overview.html", function (err) {
       if (err)
         reject(err);
       else
