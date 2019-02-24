8ea42dc184b5aa432bee7fb985d6042c00bce690
Chris Lilley <chris@w3.org>
2019-02-24 03:59:02 +0200

    add ProPhotoRGB to conversions sample code

--- css-color-4/conversions.js
+++ css-color-4/conversions.js
@@ -92,6 +92,45 @@ function XYZ_to_lin_P3(XYZ) {
 	return math.multiply(M, XYZ).valueOf();
 }
 
+// ProPhotoRGB functions
+
+function lin_ProPhoto(RGB) {
+	// convert an array of ProPhotoRGB values in the range 0.0 - 1.0
+	// to linear light (un-companded) form.
+	return RGB.map(function (val) {
+	  return Math.pow(val, 1.8);
+	});
+  }
+
+function gam_ProPhoto(RGB) {
+	// convert an array of linear-light ProPhotoRGB  in the range 0.0-1.0
+	// to gamma corrected form
+	return RGB.map(function (val) {
+		return Math.pow(val, 1/1.8);
+	});
+  }
+
+function lin_ProPhoto_to_XYZ(rgb) {
+	// convert an array of linear-light ProPhotoRGB values to CIE XYZ
+	// using  D50 (so no chromatic adaptation needed afterwards)
+	// http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+	var M = Math.matrix([
+	[ 0.7977604896723027,  0.13518583717574031,  0.0313493495815248     ],
+	[ 0.2880711282292934,  0.7118432178101014,   0.00008565396060525902 ],
+	[ 0.0,                 0.0,                  0.8251046025104601     ]
+	]);
+
+	return Math.multiply(M, rgb).valueOf();
+  }
+
+function XYZ_to_lin_ProPhoto(XYZ) {
+	// convert XYZ to linear-light ProPhotoRGB
+	var M = Math.matrix([
+  	[  1.3457989731028281,  -0.25558010007997534,  -0.05110628506753401 ],
+  	[ -0.5446224939028347,   1.5082327413132781,    0.02053603239147973 ],
+  	[  0.0,                  0.0,                   1.2119675456389454  ]
+
+
 //Rec. 2020-related functions
 
 function lin_2020(RGB) {
