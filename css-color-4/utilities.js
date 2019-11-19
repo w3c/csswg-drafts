ac931d880418c5477fb385a93a945f012de5fc19
Chris Lilley <chris@w3.org>
2019-11-19 12:16:43 -0500

    finish off the sRGB - LCH utilities

--- css-color-4/utilities.js
+++ css-color-4/utilities.js
@@ -20,7 +20,7 @@ function contrast(RGB1, RGB2) {
     var L1 = sRGB_to_luminance(RGB1);
     var L2 = sRGB_to_luminance(RGB2);
 
-    if L1 > L2 return (L1 + 0.05) / (L2 + 0.05);
+    if (L1 > L2) return (L1 + 0.05) / (L2 + 0.05);
     return (L2 + 0.05) / (L1 + 0.05);
 }
 
@@ -32,5 +32,19 @@ function sRGB_to_LCH(RGB) {
     // then convert XYZ to CIE Lab
     // and finally, convert to CIE LCH
 
-    return lin_sRGB_to_XYZ(lin_sRGB(RGB));
+    return Lab_to_LCH(XYZ_to_Lab(D65_to_D50(lin_sRGB_to_XYZ(lin_sRGB(RGB)))));
+}
+
+functions LCH_to_sRGB(LCH) {
+    // convert an array of CIE LCH values
+    // to CIE Lab, and then to XYZ,
+    // adapt from D50 to D65,
+    // then convert XYZ to linear-light sRGB
+    // and finally to gamma corrected sRGB
+    // for in-gamut colors, components are in the 0.0 to 1.0 range
+    // out of gamut colors may have negative components
+    // or components greater than 1.0
+    // so check for that :)
+
+    return gam(sRGB(XYZ_to_lin_sRGB(Lab_to_XYZ(D50_to_D65(LCH_to_Lab(LCH)))));
 }
