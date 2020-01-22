0037c0a9103e8ba5791590dc63f3a647380dcc28
Chris Lilley <chris@w3.org>
2020-01-22 16:24:34 +0100

    typo in sample code

--- css-color-4/conversions.js
+++ css-color-4/conversions.js
@@ -128,7 +128,7 @@ function lin_ProPhoto_to_XYZ(rgb) {
 	// convert an array of linear-light prophoto-rgb values to CIE XYZ
 	// using  D50 (so no chromatic adaptation needed afterwards)
 	// http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
-	var M = Math.matrix([
+	var M = math.matrix([
 	[ 0.7977604896723027,  0.13518583717574031,  0.0313493495815248     ],
 	[ 0.2880711282292934,  0.7118432178101014,   0.00008565396060525902 ],
 	[ 0.0,                 0.0,                  0.8251046025104601     ]
@@ -139,7 +139,7 @@ function lin_ProPhoto_to_XYZ(rgb) {
 
 function XYZ_to_lin_ProPhoto(XYZ) {
 	// convert XYZ to linear-light prophoto-rgb
-	var M = Math.matrix([
+	var M = math.matrix([
   	[  1.3457989731028281,  -0.25558010007997534,  -0.05110628506753401 ],
   	[ -0.5446224939028347,   1.5082327413132781,    0.02053603239147973 ],
   	[  0.0,                  0.0,                   1.2119675456389454  ]
