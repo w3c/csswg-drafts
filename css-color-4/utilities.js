3931bbcf08c9abc12592a51136eaa1ac42934d44
Chris Lilley <chris@w3.org>
2020-03-18 18:38:55 -0400

    [css-color-4] add naive conversions

--- css-color-4/utilities.js
+++ css-color-4/utilities.js
@@ -128,3 +128,42 @@ function hslToRgb(hue, sat, light) {
     else if(hue < 4) return (t2 - t1) * (4 - hue) + t1;
     else return t1;
   }
+
+// These are the naive algorithms from CS Color 4
+
+function naive_CMYK_to_sRGB(CMYK) {
+    // CMYK is an array of four values
+    // in the range [0.0, 1.0]
+    // the optput is an array of [RGB]
+    // also in the [0.0, 1.0] range
+    // because the naive algorithm does not generate out of gamut colors
+    // neither does it generate accurate simulations of practical CMYK colors
+
+    var cyan = CMYK[0], magenta = CMYK[1], yellow = CMYK[2], black = CMYK[3];
+
+    var red = 1 - Math.min(1, cyan * (1 - black) + black);
+    var green = 1 - Math.min(1, magenta * (1 - black) + black);
+    var blue = 1 - Math.min(1, yellow * (1 - black) + black);
+
+    return [red, green, blue];
+
+}
+
+function naive_sRGB_to_CMYK(RGB) {
+    // RGB is an arravy of three values
+    // in the range [0.0, 1.0]
+    // the output is an array of [CMYK]
+    // also in the [0.0, 1.0] range
+    // with maximum GCR and (I think) 200% TAC
+    // the naive algorithm does not generate out of gamut colors
+    // neither does it generate accurate simulations of practical CMYK colors
+
+    var red = RGB[0], green= RGB[1], blue = RGB[2];
+
+    var black = 1 - Math.max(red, green, blue);
+    var cyan = (black = 1.0)? 0: (1 - red - black) / (1 - black);
+    var magenta = (black = 1.0)? 0: (1 - green - black) / (1 - black);
+    var yellow = (black = 1.0)? 0: (1 - blue - black) / (1 - black);
+
+    return [cyan, magenta, yellow, black];
+}
