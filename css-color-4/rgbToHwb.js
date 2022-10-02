a790160dbb77d3b51c47c7cdd5a4117c3eab7135
Lars Grefer <github@larsgrefer.de>
2022-10-03 00:18:35 +0200

    Add JSDoc comments to color example functions

--- css-color-4/rgbToHwb.js
+++ css-color-4/rgbToHwb.js
@@ -1,3 +1,9 @@
+/**
+ * @param {number} red - Red component 0..1
+ * @param {number} green - Green component 0..1
+ * @param {number} blue - Blue component 0..1
+ * @return {number[]} Array of HWB values: Hue as degrees 0..360, Whiteness and Blackness as percentages 0..100
+ */
 function rgbToHwb(red, green, blue) {
     var hsl = rgbToHsl(red, green, blue);
     var white = Math.min(red, green, blue);
