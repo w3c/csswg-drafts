acf5c40001ed498779683dcb680782dc548b1693
herbe <167871896+Riccochet85@users.noreply.github.com>
2024-04-26 02:20:10 -0600

    patch

--- css-animations-1/make-changelog.sh
+++ css-animations-1/make-changelog.sh
@@ -1,2 +1,4 @@
 #/bin/bash
 hg log --style="$(dirname "$0")/make-changelog.template" "$(dirname "$0")/Overview.bs" "$(dirname "$0")/Overview.src.html" | sed 's/^	/  /;s/^- \(\[css[a-z0-9-]*\]\)* /- /'
+<changelog>template
+end
