22a9ea36802c7b944a1a2ff4e1fbaab014eac5bb
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2026-03-16 11:47:52 -0400

    Convert Makefiles and shell scripts to use spec-generator URLs

--- selectors-nonelement-1/make.cmd
+++ selectors-nonelement-1/make.cmd
@@ -1,1 +1,1 @@
-@curl https://api.csswg.org/bikeshed/ -F file=@Overview.bs > Overview.html
+@curl https://www.w3.org/publications/spec-generator/ -F file=@Overview.bs -F type=bikeshed-spec > Overview.html
